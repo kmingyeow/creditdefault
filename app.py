23670d8a75f66e9605c04fbc1e070b3235de9757
kmingyeow <99856247+kmingyeow@users.noreply.github.com>
2022-02-27 17:24:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,14 @@ app = Flask(__name__)
 from flask import request, render_template
 from keras.models import load_model
 import joblib
+import pandas as pd
+
+df = pd.read_csv("Credit Card Default II (balance).csv")
+
+# create scaler
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+standardized = scaler.fit_transform(df.iloc[:,0:3])
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -26,15 +34,17 @@ def index():
         Income = request.form.get("Income")
         Age = request.form.get("Age")
         Loan = request.form.get("Loan")
-        print(Income,Age, Loan)
+        print(Income, Age, Loan)
         
         model = joblib.load("MLPClassifier_CreditDefault")
-        pred = model.predict([[float(Income), float(Age), float(Loan)]])
+        pred = model.predict(scaler.transform([[float(Income), float(Age), float(Loan)]]))
         pred = pred[0]
+        
         s = "The predicted default risk is: " + str(pred)
         return(render_template("index.html", result=s))
     else:
         return(render_template("index.html", result="2"))
+
 
 
 # In[ ]:
